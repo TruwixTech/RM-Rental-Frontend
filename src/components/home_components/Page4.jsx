032cf2c6b599174c07f4e4ab139f923c432ca12a
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-10-09 14:28:48 +0530

    home page product redirect

--- src/components/home_components/Page4.jsx
+++ src/components/home_components/Page4.jsx
@@ -1,35 +1,41 @@
-import React from 'react'
-import productimg from '../../assets/img/Productimg.png'
-import '../../assets/csss/Page4.css'
+import productimg from "../../assets/img/productImg.jpg"; // Update to the refrigerator image
+import "../../assets/csss/Page4.css";
+import { Link } from "react-router-dom";
 
 const Page4 = () => {
   return (
-    <div className='page4'>
-    <div className='page4-left'>
-        <img src={productimg} alt="" />
-    </div>
-    <div className='page4-right'>
-        <h1>Crafted With Excelent Wood</h1>
-        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque  laudantium, totam rem aperiam</p>
-        <div className='page4-right-group'>
-            <div className='page4-right-group-contain'>
+    <div className="page4">
+      <div className="page4-left">
+        <img src={productimg} alt="Refrigerator" />
+      </div>
+      <div className="page4-right">
+        <h1>Modern Refrigerator with Smart Features</h1>
+        <p>
+          Experience the perfect blend of style and functionality with our
+          latest refrigerator model. It offers energy efficiency, spacious
+          storage, and advanced cooling technology for all your food
+          preservation needs.
+        </p>
+        <div className="page4-right-group">
+          <div className="page4-right-group-contain">
             <span>Materials</span>
-            <p>Naturals wood, 100% Cotton, Poluyutherine Fabric</p>
-            </div>
-            <div className='page4-right-group-contain'>
-                <span>Products size</span>
-                <p>60 cm x 168 cm x 22 cm</p>
-            </div>
-            <div className='page4-right-group-contain'>
-                <span>Available in</span>
-                <p>BlueGrey, Yellow, Red,  and Custom</p>
-            </div>
+            <p>Stainless Steel, Tempered Glass Shelves, BPA-Free Plastics</p>
+          </div>
+          <div className="page4-right-group-contain">
+            <span>Product Size</span>
+            <p>70 cm x 180 cm x 70 cm</p>
+          </div>
+          <div className="page4-right-group-contain">
+            <span>Available In</span>
+            <p>Silver, Black, White, and Custom Colors</p>
+          </div>
         </div>
-        <button>Shop Now</button>
+        <Link to={`/product/6706454bfdad55b1d313a1dc`}>
+          <button>Shop Now</button>
+        </Link>
+      </div>
     </div>
-        
-    </div>
-  )
-}
+  );
+};
 
-export default Page4
+export default Page4;
