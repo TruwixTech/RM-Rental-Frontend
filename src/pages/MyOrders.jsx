2ce6973f230359134537e1fafa1a18dc010eed8d
Suraj Narayan Darade <daradesuraj05@gmail.com>
2024-10-10 19:21:31 +0530

    remove setting button

--- src/pages/MyOrders.jsx
+++ src/pages/MyOrders.jsx
@@ -119,7 +119,7 @@ const MyOrders = () => {
                 name: "Payment",
                 url: "/payment",
               },
-              { icon: <IoSettings />, name: "Setting", url: "/setting" },
+              // { icon: <IoSettings />, name: "Setting", url: "/setting" },
             ].map((item, index) => (
               <Link
                 to={item.url}
